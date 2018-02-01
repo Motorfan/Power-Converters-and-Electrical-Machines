75d5de28b2a86554301e8f3c0b60db038ef8169a
Görkem ŞAHİNLER <gorkem.sahinler@gmail.com>
2018-02-01 23:26:37 +0300

    Correction - Simscape Model of Open Loop Buck Con.

---
+++ Modelling and Control of Power Electronics Systems/Buck Converter/DC-DC Buck Converter CPM Open Loop/buck_converter_CPM_open_loop_simscape_parameters.m
@@ -0,0 +1,34 @@
+Vg=5;
+Vo=2;
+Rload=0.4;
+L=1e-6;
+C=200e-6;
+Resr=0.8e-3;
+RL=15e-3;
+D=Vo/Vg;
+Io=Vo/Rload;      
+Rs=0.1;          % Current sensing resistance
+fs=1e6;          % Switching frequency for the converter
+Dmax=0.9;
+Dmin=0;
+Vcmax=5;
+Voffset=0;
+Vcc=5;
+iLmax=5;
+Rin=10e6;   
+H=0.6;
+
+
+%% Bode plot for uncompensated loop
+
+%Gvc=vo^/vc^=(1/Rf)*[Rload||(Resr+1/sC)]
+%Tu=H*Gvc
+s=tf('s')
+Tu=(H/Rs)*[Rload*(Resr+1/(s*C))/(Rload+Resr+1/(s*C))];
+
+bode(Tu)
+legend('y = Tu')
+
+
+
+
